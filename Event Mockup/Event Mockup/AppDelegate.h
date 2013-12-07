7b4eb22b4b251a76f4290674c2d70fa510abbcbf
Hai Nguyen <nhuhai.h.nguyen@gmail.com>
2013-12-07 12:01:02 -0800

    initial commit

---
+++ Event Mockup/Event Mockup/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Event Mockup
+//
+//  Created by Hai Nguyen on 12/7/13.
+//  Copyright (c) 2013 Hai Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Event Mockup/Event MockupTests/Event_MockupTests.m
@@ -0,0 +1,34 @@
+//
+//  Event_MockupTests.m
+//  Event MockupTests
+//
+//  Created by Hai Nguyen on 12/7/13.
+//  Copyright (c) 2013 Hai Nguyen. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Event_MockupTests : XCTestCase
+
+@end
+
+@implementation Event_MockupTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
